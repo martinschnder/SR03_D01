8623a49430f6f61ad380aabc9d56783799ffeb64
oceane <oceane.bordeau@etu.utc.fr>
2022-03-28 13:41:15 +0200

    com server

--- serverPackage/Server.java
+++ serverPackage/Server.java
@@ -1,17 +1,30 @@
-package server;
+package serverPackage;
 
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 
-public class Serveur {
+/**
+ * Server of the chat
+ * 
+ * @author Martin Schneider
+ * @author Océane Bordeau
+ */
+public class Server {
+
+    /**
+     * Our main server method, waiting for new clients and starting a server thread
+     * 
+     * @param args command line arguments
+     * @exception Exception any error occured in main
+     */
     public static void main(String[] args) {
         ArrayList<ServerThread> threadList = new ArrayList<>();
         try {
-            ServerSocket serveurSocket = new ServerSocket(20000);
+            ServerSocket serverSocket = new ServerSocket(20000);
             while (true) {
                 System.out.println("Waiting for new client...");
-                Socket socket = serveurSocket.accept();
+                Socket socket = serverSocket.accept();
                 System.out.println("A new client has entered.");
                 ServerThread serverThread = new ServerThread(socket, threadList);
                 threadList.add(serverThread);
--- serverPackage/ServerThread.java
+++ serverPackage/ServerThread.java
@@ -1,4 +1,4 @@
-package server;
+package serverPackage;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -7,41 +7,81 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.ArrayList;
 
+/**
+ * Thread of the server
+ * Intercept messages send in the socket
+ * 
+ * @author Martin Schneider
+ * @author Océane Bordeau
+ */
 public class ServerThread extends Thread {
     private Socket socket;
     private ArrayList<ServerThread> threadList;
     private PrintWriter output;
 
+    /**
+     * Default ServerThread constructor
+     * 
+     * @param socket     a socket connected with the client
+     * @param threadList list of the thread created
+     * @throws IOException failure or interrupted I/O operations
+     */
     public ServerThread(Socket socket, ArrayList<ServerThread> threadList) throws IOException {
         setSocket(socket);
         setThreadList(threadList);
         setOutput(new PrintWriter(socket.getOutputStream(), true));
     }
 
+    /**
+     * @return the current socket
+     */
     public Socket getSocket() {
         return socket;
     }
 
+    /**
+     * @return the current list of threads
+     */
     public ArrayList<ServerThread> getThreadList() {
         return threadList;
     }
 
+    /**
+     * @return the current output of the server
+     */
     public PrintWriter getOutput() {
         return output;
     }
 
+    /**
+     * @param socket set the socket for the current class
+     */
     public void setSocket(Socket socket) {
         this.socket = socket;
     }
 
+    /**
+     * @param threadList set the list of threads for the current class
+     */
     public void setThreadList(ArrayList<ServerThread> threadList) {
         this.threadList = threadList;
     }
 
+    /**
+     * @param output set the output for the current class
+     */
     public void setOutput(PrintWriter output) {
         this.output = output;
     }
 
+    /**
+     * <strong>Processing of the thread when it starts</strong>
+     * Disconnect the socket if the input is "exit"
+     * Change the thread name if the input is "I want to change my name"
+     * Otherwise, print the input of the socket to all the others sockets
+     * 
+     * @exception IOException failure or interrupted I/O operations
+     */
     @Override
     public void run() {
         try {
@@ -69,6 +109,11 @@ public class ServerThread extends Thread {
         }
     }
 
+    /**
+     * Print the string to all but the current socket
+     * 
+     * @param outputString string to be printed
+     */
     private void printToAllClients(String outputString) {
         for (ServerThread sT : getThreadList()) {
             if (sT != this) {
@@ -77,6 +122,14 @@ public class ServerThread extends Thread {
         }
     }
 
+    /**
+     * Change the name of the current thread
+     * Check if the name is not already use
+     * 
+     * @param name integer variable set to 0 if the client has no name already, and
+     *             more than 0 if he has already one
+     * @exception Exception any error occured in the function
+     */
     private void changeName(Integer name) {
         try {
             BufferedReader input = new BufferedReader(new InputStreamReader(getSocket().getInputStream()));
@@ -84,22 +137,40 @@ public class ServerThread extends Thread {
             String clientName = "";
             Boolean noClientName = true;
             while (noClientName) {
+                /**
+                 * while the client has not change his name
+                 */
                 getOutput().println("Enter your name :");
                 userInput = input.readLine();
                 clientName = userInput;
                 noClientName = false;
                 for (ServerThread sT : getThreadList()) {
+                    /**
+                     * check all the thread's name in the thread's list
+                     */
                     if (sT.getName().equals(clientName)) {
+                        /**
+                         * exit from the for loop if the name already use, to start again the while loop
+                         */
                         noClientName = true;
                         break;
                     }
                 }
                 if (!noClientName) {
+                    /**
+                     * the name is not already use, we change the thread's name
+                     */
                     if (name > 0) {
+                        /**
+                         * not the first change of name
+                         */
                         String outputString = getName() + " has changed his name to " + clientName;
                         printToAllClients(outputString);
                         setName(clientName);
                     } else {
+                        /**
+                         * first change of name
+                         */
                         setName(clientName);
                         getOutput().println("-------------------------");
                         String outputString = getName() + " has join the chat";
